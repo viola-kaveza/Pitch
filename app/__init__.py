dde8d2cc907bcf515365bc587c560d53940b272f
viola-kaveza <viola.kaveza@student.moringaschool.com>
2021-09-27 18:43:55 +0300

    Refactor:code

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '0727325535'
+app.config['SECRET_KEY'] = '0796073190'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
